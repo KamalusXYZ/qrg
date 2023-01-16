e0613ddcc18badebda1ec47be0169e52fe775cbb
KamalusXYZ <k.haddou123@gmail.com>
2023-01-16 16:33:26 +0100

    Mise à jour pour corriger des bugs de connexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -29,7 +29,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+            $user->setCreateDateTime(date_create('now'));
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -45,10 +45,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+         return new RedirectResponse($this->urlGenerator->generate('app_main'));
     }
 
     protected function getLoginUrl(Request $request): string
