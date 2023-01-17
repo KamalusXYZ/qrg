6db9565c3c420fdcc5f95685876f13699fbc3e4a
KamalusXYZ <k.haddou123@gmail.com>
2023-01-17 10:33:15 +0100

    maj legere :Mise en place d'un systeme de promotion vers publicateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,10 +9,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
